80e42c1e69f29dff2b46c72dc11bce3fd7cac8f7
johnyu763 <johnyu763@gmail.com>
2022-10-05 22:23:01 -0700

    Updates

---
+++ precache-manifest.209492e874c11afe67d477a737f48526.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10041a461c39afb15ea38758be1beb0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c01035eaefd1bd10b456",
+    "url": "./static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "f6861554d27e134286a2",
+    "url": "./static/css/main.2cdf4ffd.chunk.css"
+  },
+  {
+    "revision": "c01035eaefd1bd10b456",
+    "url": "./static/js/2.c737f39e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.c737f39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6861554d27e134286a2",
+    "url": "./static/js/main.3104f6b0.chunk.js"
+  },
+  {
+    "revision": "9cdba1afc0bf5239a652",
+    "url": "./static/js/runtime-main.49b401d1.js"
+  },
+  {
+    "revision": "e43494f5367f82de69f28089fcd25d5b",
+    "url": "./static/media/JohnAltPortrait.e43494f5.jpg"
+  },
+  {
+    "revision": "99a7e17bb0279774b7de124ca794cac2",
+    "url": "./static/media/emailicon.99a7e17b.png"
+  },
+  {
+    "revision": "a0a0d85802e031d9a7a51ee034eea081",
+    "url": "./static/media/linkedinicon.a0a0d858.png"
+  },
+  {
+    "revision": "7e0aa13ecbebc625d1115ca41aa48b95",
+    "url": "./static/media/ucsclogo.7e0aa13e.png"
+  }
+]);
